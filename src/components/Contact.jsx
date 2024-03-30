b256edb5868cae9c8a49eaaf18024aab522f1428
Vitor Hugo Silva Gonçalves <vitorhugo662@hotmail.com>
2024-03-30 13:28:15 -0300

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,29 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap';
 import emailjs from '@emailjs/browser';
 import maps_image from './imagens/maps.jfif';
 import phone_image from './imagens/phone.jfif';
 import mail_image from './imagens/mail.jfif';
 
+const serviceId = 'service_xtcjer6';
+const templateId = 'template_jfq6bu5';
+const publicKey ='56ZSdEbeE-Y1k3iZK';
+
 function Contact() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [emailSent, setEmailSent] = useState(false);
+  const [error, setError] = useState('');
+  const [validationError, setValidationError] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const serviceId = 'service_xtcjer6';
-    const templateId = 'template_jfq6bu5';
-    const publicKey ='56ZSdEbeE-Y1k3iZK';
+    if (!name || !email || !message) {
+      setValidationError('Please fill in all fields.');
+      return;
+    }
 
     const templateParams = {
       form_name: name,
@@ -30,9 +38,15 @@ function Contact() {
         setName('');
         setEmail('');
         setMessage('');
+        setEmailSent(true);
+        setError('');
+        setValidationError('');
       })
       .catch((error) => {
         console.error('Error sending email:', error);
+        setError('Error sending email. Please try again later.');
+        setEmailSent(false);
+        setValidationError('');
       });
   };
 
@@ -49,6 +63,9 @@ function Contact() {
               <p><img src={mail_image} alt="Mail Icon" width="25" height="25"/> vitor.goncalves@ufu.br</p>
             </Col>
             <Col>
+              {emailSent && <Alert variant="success">Email sent successfully!</Alert>}
+              {error && <Alert variant="danger">{error}</Alert>}
+              {validationError && <Alert variant="danger">{validationError}</Alert>}
               <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="name">
                   <Form.Control type="text" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} /><br />
